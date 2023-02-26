70eb67e2c410ef098067f7b0d2401792b01ba9b2
KombinatorVit <slonik0775@gmail.com>
2023-02-26 16:23:41 +0200

    фильтрация, удаление, добавление, работа с анимацией

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 
 import App from './components/app/App';
 import store from './store';
@@ -8,11 +8,10 @@ import store from './store';
 import './styles/index.scss';
 
 ReactDOM.render(
-  <React.StrictMode>
     <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+        <App/>
+    </Provider>,
+document.getElementById('root')
+)
+;
 
